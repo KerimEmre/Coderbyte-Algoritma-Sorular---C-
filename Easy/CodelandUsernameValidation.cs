06a72d09e049522cd6db9003b6d95156a2a2977e
Kerim Emre Kurt <themre098@gmail.com>
2024-06-07 23:11:02 +0300

    Create CodelandUsernameValidation.cs

---
+++ Easy/CodelandUsernameValidation.cs
@@ -0,0 +1,45 @@
+/*
+Have the function CodelandUsernameValidation(str) take the str parameter being passed and determine if the string is a valid username according to the following rules:
+
+1. The username is between 4 and 25 characters.
+2. It must start with a letter.
+3. It can only contain letters, numbers, and the underscore character.
+4. It cannot end with an underscore character.
+
+If the username is valid then your program should return the string true, otherwise return the string false.
+
+*/
+
+using System;
+using System.Text.RegularExpressions;
+
+class MainClass {
+
+  public static string CodelandUsernameValidation(string str) {
+
+    if (str.Length < 4 
+    || str.Length > 25 
+    || str.Substring(str.Length-1,1) =="_"
+    || !(Regex.IsMatch(str, @"^[a-zA-Z0-9_]+$"))
+    || !(Regex.IsMatch(str.Substring(0,1), @"^[a-zA-Z]+$")) )
+    {
+      str="false";
+      }
+    else 
+    {
+      str="true";
+      }
+    
+    
+    return str;
+
+  }
+
+  static void Main() {  
+
+    // keep this function call here
+    Console.WriteLine(CodelandUsernameValidation(Console.ReadLine()));
+    
+  } 
+
+}
